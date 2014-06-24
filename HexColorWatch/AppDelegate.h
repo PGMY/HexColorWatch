d30e1e4ee5c4ffee859eb9e1d7b2350cdb79894d
PGMY <PGMYURI@gmail.com>
2014-06-24 10:18:29 +0900

    Initial Commit

---
+++ HexColorWatch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HexColorWatch
+//
+//  Created by Mika Yamamoto on 2014/06/24.
+//  Copyright (c) 2014年 Mika Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
